e213ae5f8bb537d8478a2dbc6ee047a93ea234a6
Serban Constantinescu <serban.constantinescu@arm.com>
2013-10-20 15:57:34 +0100

    AArch64: Fix autogenerated unistd.h
    The bionic kernel header script gets confused by the definitions
    around BITS_PER_LONG ifdefs in unistd.h. This fix will help the script
    generate the desired output.
    
    Change-Id: Ib0fd72142c63e60daca527eca94cb456592c49e0
    Signed-off-by: Serban Constantinescu <serban.constantinescu@arm.com>

--- original/uapi/asm-generic/unistd.h
+++ original/uapi/asm-generic/unistd.h
@@ -15,7 +15,7 @@
 #define __SYSCALL(x, y)
 #endif
 
-#if __BITS_PER_LONG == 32 || defined(__SYSCALL_COMPAT)
+#if (__BITS_PER_LONG == 32) || defined(__SYSCALL_COMPAT)
 #define __SC_3264(_nr, _32, _64) __SYSCALL(_nr, _32)
 #else
 #define __SC_3264(_nr, _32, _64) __SYSCALL(_nr, _64)
@@ -869,7 +869,7 @@ __SYSCALL(__NR_fork, sys_ni_syscall)
  * Here we map the numbers so that both versions
  * use the same syscall table layout.
  */
-#if __BITS_PER_LONG == 64 && !defined(__SYSCALL_COMPAT)
+#if (__BITS_PER_LONG == 64) && !defined(__SYSCALL_COMPAT)
 #define __NR_fcntl __NR3264_fcntl
 #define __NR_statfs __NR3264_statfs
 #define __NR_fstatfs __NR3264_fstatfs
